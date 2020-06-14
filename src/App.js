933d0feafa500f0ff23be747820676097a75a584
Bataa123 <bataaboldbaatar1@gmail.com>
2020-06-15 00:37:23 +0800

    dunguj nuur l bolson bga

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import AboutUs from './components/aboutUs/aboutUs'
+// import logo from './images&icons/puujin.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="opacity">
+        <div className="firstLook">
+          {/* <img style={{position: 'absolute', margin: '40px 0px 0px 59px'}} src={logo}></img> */}
+          <div style={{ textAlign: "center" }}>
+            <div className="linkButtons">Home</div>
+            <div className="linkButtons">About Us</div>
+            <div className="linkButtons">Sponser</div>
+            <div className="linkButtons">University</div>
+            <div className="linkButtons">Donate</div>
+            <div className="linkButtons">Contact Us</div>
+          </div>
+          <div className='firstRectangle'>20000M Above the <br /> Gobi desert</div>
+          <div className="arrowCenter"> <div className="arrow down"></div></div>
+        </div>
+      </div>
+
+      <AboutUs/>
+    </>
   );
 }
 
---
+++ src/components/aboutUs/aboutUs.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './aboutUs.scss'
+
+const AboutUs = () => {
+    return (
+        <>
+            <div className="headLine">ABOUT US</div>
+            <div className="zuraas"></div>
+            <div className="touching">
+                <div className="usCircle">Project</div>
+                <div className='zuraasT'></div>
+                <div className="usCircle"> Goal</div>
+                <div className='zuraasT'></div>
+                <div className="usCircle"> Timeline</div>
+                <div className='zuraasT'></div>
+                <div className="usCircle"> Team</div>
+            </div>
+        </>
+    )
+}
+
+export default AboutUs;
