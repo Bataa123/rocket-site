ab2b3e57c9f0f51a2af5cb6ae228c557630e3891
Khongorzul <hongorod09@gmail.com>
2020-07-01 15:04:16 +0800

    oursponser old changes

--- src/components/ourSponser/ourSponser.jsx
+++ src/components/ourSponser/ourSponser.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./ourSponser.scss";
-import spacex from '../../images&icons/spacex.png'
+import spacex from "../../images&icons/spacex.png";
 
 const OurSponser = () => {
   return (
@@ -8,10 +8,30 @@ const OurSponser = () => {
       <div className="headLine">OUR SPONSER</div>
       <div className="zuraas"></div>
       <div className="touching">
-      <img style={{position: 'absolute', margin: '40px 0px 0px 59px'}} src={spacex}></img>
-        <div className="zuraasT"></div>
-        <div className="zuraasT"></div>
-        <div className="zuraasT"></div>
+        <img
+          style={{
+            margin: "40px 0px 0px 59px",
+            height: "32px",
+            width: "240px"
+          }}
+          src={spacex}
+        ></img>
+        <img
+          style={{
+            margin: "40px 0px 0px 59px",
+            height: "32px",
+            width: "240px"
+          }}
+          src={spacex}
+        ></img>
+        <img
+          style={{
+            margin: "40px 0px 0px 59px",
+            height: "32px",
+            width: "240px"
+          }}
+          src={spacex}
+        ></img>
       </div>
     </>
   );
