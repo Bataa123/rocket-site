9a3d7967f51f228a81a5742775b365a9967542f6
soulforger <n.zolbooyuro@gmail.com>
2020-08-09 16:19:02 +0800

    added ages and some responsibility

--- src/components/timeLine/timeLine.jsx
+++ src/components/timeLine/timeLine.jsx
@@ -3,7 +3,9 @@ import "./timeLine.scss";
 
 export const TimeLine = () => {
   window.onload = () => {
-    for (var i = 0; i < 60; i++) {
+    const width = window.innerWidth;
+    const numberOfDot = width / 30;
+    for (var i = 0; i < numberOfDot; i++) {
       const dot = document.createElement("div");
       dot.className = "dot";
       document.getElementById("dot-container").appendChild(dot);
@@ -17,22 +19,27 @@ export const TimeLine = () => {
         <div className="timeLineTexts">
           <div className="timeLineText">
             Project starts
+            <div>2020.2.22</div>
             <div className="line"></div>
           </div>
           <div className="timeLineText">
             Calculation of simulation
+            <div>2020.3.25 -- 2020.7.11</div>
             <div className="line"></div>
           </div>
           <div className="timeLineText">
             Static test #1
+            <div>2020.11.5</div>
             <div className="line"></div>
           </div>
           <div className="timeLineText">
             Static test #3
+            <div>2020.11.20</div>
             <div className="line"></div>
           </div>
           <div className="timeLineText">
             Launch
+            <div>2020.11.30</div>
             <div className="line"></div>
           </div>
         </div>
@@ -40,18 +47,22 @@ export const TimeLine = () => {
         <div className="timeLineTexts">
           <div className="timeLineText">
             <div className="line"></div>
+            <div>2020.3.22</div>
             Formed a team
           </div>
           <div className="timeLineText">
             <div className="line"></div>
+            <div>2020.11.1</div>
             Complete the assembly
           </div>
           <div className="timeLineText">
             <div className="line"></div>
+            <div>2020.11.10</div>
             Static test #2
           </div>
           <div className="timeLineText">
             <div className="line"></div>
+            <div>2020.11.28</div>
             Launch site will be ready
           </div>
         </div>
