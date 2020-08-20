2f939b56804e774a4c49f261ecbc809b8f88693b
Bataa123 <bataaboldbaatar1@gmail.com>
2020-08-20 13:28:33 +0800

    dahin pushlev

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -4,10 +4,14 @@ import './navigation.scss'
 export const Navigation = () => {
     return (
         <div className="nav">
-            <div className="text">
+            <div className="text padding">
                 Gobi Space
             </div>
-            <div className="menu"></div>
+            <svg className="padding" width="20" height="14" viewBox="0 0 20 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect width="20" height="2" rx="1" fill="white"/>
+                <rect y="6" width="20" height="2" rx="1" fill="white"/>
+                <rect y="12" width="20" height="2" rx="1" fill="white"/>
+            </svg>
         </div>
     )
 }
