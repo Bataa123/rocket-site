711acf2d94388fcee74a89882b04e83f4d6e2276
Bataa123 <bataaboldbaatar1@gmail.com>
2020-08-20 12:47:46 +0800

    yaaraltai push

---
+++ src/components/navigation/navigation.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './navigation.scss'
+
+export const Navigation = () => {
+    return (
+        <div className="nav">
+            <div className="text">
+                Gobi Space
+            </div>
+            <div className="menu"></div>
+        </div>
+    )
+}
