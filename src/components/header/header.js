2fe180ee18f11bdafa858be9043792c23ed65b00
soulforger <n.zolbooyuro@gmail.com>
2020-08-30 01:56:30 +0800

    update: new looking navbar

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,34 +1,16 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
 import './header.scss'
 import { Navigation } from '../navigation/navigation'
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 
 export const Header = () => {
 
     return (
         <div className="opacity">
-            {/* <video autoPlay loop muted style={{position: 'absolute', width: '100vw', height: '100vh', objectFit: 'cover'}}>
-                <source src={Video} type='video/mp4'></source>
-            </video> */}
-            {/* <div className='logo'></div> */}
             <div className="firstLook">
                 <Navigation />
-                {/* <div style={{ textAlign: "center" }}>
-                    <div className="linkButtons" onClick={() =>
-                        document.getElementById('aboutUs').scrollIntoView({ behavior: "smooth" })
-                    }>About Us</div>
-                    <div className="linkButtons" onClick={() =>
-                        document.getElementById('sponser').scrollIntoView({ behavior: "smooth" })
-                    }>Sponser</div>
-                    <div className="linkButtons" onClick={() =>
-                        document.getElementById('donation').scrollIntoView({ behavior: "smooth" })
-                    }>Donate</div>
-                    <div className="linkButtons" onClick={() =>
-                        document.getElementById('contact').scrollIntoView({ behavior: "smooth" })
-                    }>Contact Us</div>
-                    <Link className="linkButtons" to="/ourTeam">Our Team</Link>
-                </div> */}
                 <div className='firstRectangle'>20000M Above the <br /> Gobi desert</div>
+                <KeyboardArrowDownIcon className="headerArrow"/>
             </div>
         </div>
     )
---
+++ src/components/navigation/topBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const TopBar = () => {
+  return (
+    <div className="topBarContainer">
+      <div className="topBarLinks">home</div>
+      <div className="topBarLinks">project</div>
+      <div className="topBarLinks">goal</div>
+      <div className="topBarLinks">team</div>
+      <div className="topBarLinks">timeline</div>
+      <div className="topBarLinks">sponsor</div>
+      <div className="topBarLinks">donate</div>
+      <div className="topBarLinks">contact us</div>
+    </div>
+  );
+};
