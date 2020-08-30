f3a0a1ab58cce9357dd85368541583facc704ec4
soulforger <n.zolbooyuro@gmail.com>
2020-08-30 23:26:52 +0800

    feat: background images added

--- src/components/ourSponsor/ourSponsor.jsx
+++ src/components/ourSponsor/ourSponsor.jsx
@@ -1,28 +1,33 @@
 import React from "react";
 import "./ourSponsor.scss";
-import miat from '../../images&icons/sponsor/miatSponsor.png'
-import mosta from '../../images&icons/sponsor/mostaSponsor.png'
-import nomt from '../../images&icons/sponsor/nomtSponsor.png'
+import miat from "../../images&icons/sponsor/miatSponsor.png";
+import mosta from "../../images&icons/sponsor/mostaSponsor.png";
+import nomt from "../../images&icons/sponsor/nomtSponsor.png";
+import background from "../../images&icons/backgrounds/sponsorBackground.svg";
+import background1 from "../../images&icons/backgrounds/sponsorBackground1.svg";
 
 const OurSponsor = () => {
   return (
-    <div className="ourSponsor">
-      <div id="sponsor" className="headLine"> SPONSORS</div>
+    <div className="ourSponsor" style={{ position: "relative" }}>
+      <div id="sponsor" className="headLine">
+        SPONSORS
+      </div>
       <div className="zuraas"></div>
       <div className="sponsor">
-        <img
-          alt="number2"
-          src={miat}
-        ></img>
-        <img
-          alt="number1"
-          src={nomt}
-        ></img>
-        <img
-          alt="number3"
-          src={mosta}
-        ></img>
+        <img alt="number2" src={miat}></img>
+        <img alt="number1" src={nomt}></img>
+        <img alt="number3" src={mosta}></img>
       </div>
+      <img
+        alt="sponsorBackground"
+        src={background}
+        className="sponsorBackground"
+      />
+      <img
+        alt="sponsorBackground1"
+        src={background1}
+        className="sponsorBackground1"
+      />
     </div>
   );
 };
