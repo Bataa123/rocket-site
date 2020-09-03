9660b1b03d96b146a0b730e1637d0d1fcad02fa6
zolboo <zolboo.intern@andsystems.net>
2020-09-03 18:23:23 +0800

    switch: image to text

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './header.scss'
 import { Navigation } from '../navigation/navigation'
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import headerFrontImage from '../../images&icons/backgrounds/headerFrontImage.svg'
 
 export const Header = () => {
 
@@ -9,7 +10,8 @@ export const Header = () => {
         <div className="opacity">
             <div className="firstLook">
                 <Navigation />
-                <div className='firstRectangle'>20000M Above the <br /> Gobi desert</div>
+                {/* <div className='firstRectangle'>20000M Above the <br /> Gobi desert</div> */}
+                <img src={headerFrontImage} alt="frontImage" className='firstRectangle'/>
                 <KeyboardArrowDownIcon className="headerArrow"/>
             </div>
         </div>
