7dcdad9fd9f331727bf96dbb3334a1251216ecbb
soulforger <n.zolbooyuro@gmail.com>
2020-09-04 22:13:09 +0800

    feat: our Team section done

--- src/components/ourTeam/ourTeam.jsx
+++ src/components/ourTeam/ourTeam.jsx
@@ -3,7 +3,7 @@ import "./ourTeam.scss";
 import background from "../../images&icons/backgrounds/ourTeamBackground.svg";
 import { useHistory } from "react-router-dom";
 import { OurTeamData } from "./ourTeamData";
-import cancel from "../../images&icons/linkedIcons/cancel.svg"
+import cancel from "../../images&icons/linkedIcons/cancel.svg";
 
 export const OurTeam = () => {
   const history = useHistory();
@@ -21,6 +21,7 @@ export const OurTeam = () => {
   const toTeam = () => history.push("/ourTeam");
 
   const popUp = (number) => {
+    const popUpFather = document.createElement("div");
     const popUpChild = document.createElement("div");
     const popUpImage = document.createElement("img");
     const popUpIcon = document.createElement("img");
@@ -29,8 +30,10 @@ export const OurTeam = () => {
     const popUpText1 = document.createElement("p");
     const popUpText2 = document.createElement("p");
 
-    popUpChild.className = "popUp";
-    popUpChild.id = "";
+    popUpFather.className = "popUpFather";
+
+    popUpChild.className = "popUpMember";
+    popUpChild.id = "popUpMember";
 
     popUpImage.src = OurTeamData.images[number];
     popUpImage.alt = number;
@@ -39,9 +42,10 @@ export const OurTeam = () => {
     popUpIcon.src = cancel;
     popUpIcon.alt = "cancel";
     popUpIcon.className = "popCancel";
+    popUpIcon.style.cursor = "pointer";
     popUpIcon.onclick = () => {
       document.getElementById("ourTeam").removeChild(popUpChild);
-    }
+    };
 
     popUpTitle.textContent = OurTeamData.informations[number].name;
     popUpTitle.className = "popName";
@@ -61,15 +65,21 @@ export const OurTeam = () => {
     popUpChild.appendChild(popUpText1);
     popUpChild.appendChild(popUpText2);
     popUpChild.appendChild(popUpIcon);
-    
 
     document.getElementById("ourTeam").appendChild(popUpChild);
+    document
+      .getElementById("popUpMember")
+      .scrollIntoView({ behavior: "smooth" });
   };
 
   const addMember = () => {
     const collection = members.map((cur, index) => {
       return (
-        <div onClick={() => popUp(cur)} style={{ cursor: "pointer" }} key={index}>
+        <div
+          onClick={() => popUp(cur)}
+          style={{ cursor: "pointer" }}
+          key={index}
+        >
           <img alt="2" src={OurTeamData.images[cur]} className="memberImg" />
           <div className="memberName">{OurTeamData.informations[cur].name}</div>
           <div className="memberRole">
@@ -85,8 +95,8 @@ export const OurTeam = () => {
       <div className="secondLine"></div>
       <div className="ourTeamTitle">Our Team</div>
       <div className="ourTeamText">
-        20+ Students From Across The globe, ranging from High Schooaml Students to
-        Ph.D. candidates at some of the world’s most prestigious universities
+        20+ Students From Across The globe, ranging from High Schooaml Students
+        to Ph.D. candidates at some of the world’s most prestigious universities
       </div>
       <img
         style={{ position: "absolute", zIndex: "1" }}
